fb4599c18468eb83d8d0d79616453ada1a87c345
Briliantcoin <briliantcoin@gmail.com>
2021-12-25 00:57:00 +0200

    Update Trader.sol

--- contracts/Trader.sol
+++ contracts/Trader.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.5.1;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.7.0;
 
 interface IUniswapV2Router {
     function swapETHForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
