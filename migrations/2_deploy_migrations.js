0652706a9c70795455aa58a50a036ada691c4aa0
Briliantcoin <briliantcoin@gmail.com>
2021-12-25 00:09:17 +0200

    Update 2_deploy_migrations.js

--- migrations/2_deploy_migrations.js
+++ migrations/2_deploy_migrations.js
@@ -1,7 +1,8 @@
 const Trader = artifacts.require("Trader");
 const { uniswap: addresses } = require("../addresses/index");
 const { WETH } = require("@uniswap/sdk");
+const DAIAddress = '0xaD6D458402F60fD3Bd25163575031ACDce07538D';
 
 module.exports = async function (deployer, network, accounts) {
-  await deployer.deploy(Trader, addresses.factory, WETH[1].address);
+  await deployer.deploy(Trader, addresses.router, DAIAddress);
 };
